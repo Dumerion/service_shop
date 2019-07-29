3c72ded2b1cdb1a2641686bfc808fec01f86762f
dumerion <dumerion@gmail.com>
2019-07-29 18:23:59 +0300

    Updates

---
+++ precache-manifest.53dd90de6368b61d644ac50a87fe622e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca4828c73c95f0f8d80b35af05dab35",
+    "url": "/service_shop/index.html"
+  },
+  {
+    "revision": "635f6626b28779fe411d",
+    "url": "/service_shop/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/service_shop/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "635f6626b28779fe411d",
+    "url": "/service_shop/static/js/main.ae377539.chunk.js"
+  },
+  {
+    "revision": "b7be2c8c1e7b60fbd392",
+    "url": "/service_shop/static/js/runtime~main.45fe932a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/service_shop/static/media/logo.5d5d9eef.svg"
+  }
+]);
